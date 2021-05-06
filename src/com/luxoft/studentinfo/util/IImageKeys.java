cea94ab32faadf31e3c17b266c7ca75e35270601
DAushev <DAushev@DAushev-NB.luxoft.com>
2021-05-06 14:28:48 +0300

    Added icons to context menu

--- src/com/luxoft/studentinfo/util/IImageKeys.java
+++ src/com/luxoft/studentinfo/util/IImageKeys.java
@@ -1,11 +1,6 @@
 package com.luxoft.studentinfo.util;
 
 public interface IImageKeys {
-
-	public static final String ADD = "icons/add.png";
-	public static final String DELETE = "icons/delete.png";
-	public static final String OPEN = "icons/open.png";
-	public static final String SAVE = "icons/save.png";
-	public static final String FOLDER = "icons/folder.png";
-	public static final String FILE = "icons/file.png";
+	public static final String FOLDER = "icons/folder20x20.png";
+	public static final String FILE = "icons/file20x20.png";
 }
