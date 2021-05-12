fc2ba4deaf8b60a442f1f5780b09a096ab4fa06c
DAushev <DAushev@DAushev-NB.luxoft.com>
2021-05-12 10:38:34 +0300

    Added closing editor after deleting student

--- src/com/luxoft/studentinfo/handler/DeleteHandler.java
+++ src/com/luxoft/studentinfo/handler/DeleteHandler.java
@@ -4,9 +4,12 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.luxoft.studentinfo.model.Entry;
+import com.luxoft.studentinfo.model.Student;
+import com.luxoft.studentinfo.view.InfoEditorInput;
 import com.luxoft.studentinfo.view.ViewManager;
 
 public class DeleteHandler extends AbstractHandler {
@@ -20,6 +23,11 @@ public class DeleteHandler extends AbstractHandler {
 		if (entry != null) {
 			entry.getParent().removeEntry(entry);
 			ViewManager.getInstance().getTreeViewer().refresh();
+		}
+		if (entry instanceof Student) {
+			Student student = (Student) entry;
+			IEditorPart part = HandlerUtil.getActiveSite(event).getPage().findEditor(new InfoEditorInput(student));		
+			HandlerUtil.getActiveSite(event).getPage().closeEditor(part, false);
 		}
 		return null;
 	}
