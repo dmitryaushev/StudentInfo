f48192ff8eec07c3d034972e622306dfd564cdb5
DAushev <DAushev@DAushev-NB.luxoft.com>
2021-05-18 11:33:30 +0300

    Added ability to change student's name in the editor

--- src/com/luxoft/studentinfo/view/InfoEditor.java
+++ src/com/luxoft/studentinfo/view/InfoEditor.java
@@ -1,7 +1,13 @@
 package com.luxoft.studentinfo.view;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -17,11 +23,12 @@ import org.eclipse.ui.part.EditorPart;
 
 import com.luxoft.studentinfo.model.Student;
 
-public class InfoEditor extends EditorPart{
+public class InfoEditor extends EditorPart {
 
 	public static final String ID = "com.luxoft.studentInfo.InfoEditor";
 	private Student student;
-	
+	private boolean isDirty;
+
 	private Composite parent;
 	private Text nameText;
 	private Text groupText;
@@ -30,14 +37,26 @@ public class InfoEditor extends EditorPart{
 	private Text resultText;
 	private Canvas canvas;
 
+	private List<String> undoStack;
+	private List<String> redoStack;
+
 	public InfoEditor() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-
+		String newName = nameText.getText();
+		if (!newName.trim().isEmpty()) {
+			student.setName(newName);
+			setPartName(newName);
+			ViewManager.getInstance().getTreeViewer().refresh();
+			isDirty = false;
+			firePropertyChange(PROP_DIRTY);
+		} else {
+			MessageDialog.openError(parent.getShell(), "Empty name", "Name should not be empty");
+			monitor.setCanceled(true);
+		}
 	}
 
 	@Override
@@ -56,8 +75,7 @@ public class InfoEditor extends EditorPart{
 
 	@Override
 	public boolean isDirty() {
-		// TODO Auto-generated method stub
-		return false;
+		return isDirty;
 	}
 
 	@Override
@@ -69,17 +87,21 @@ public class InfoEditor extends EditorPart{
 	@Override
 	public void createPartControl(Composite parent) {
 		this.parent = parent;
+		undoStack = new LinkedList<>();
+		undoStack.add(0, student.getName());
+		redoStack = new LinkedList<>();
 		createInputComposite();
 		createPhotoComposite();
+		addListeners();
 	}
 
 	@Override
 	public void setFocus() {
 		parent.setFocus();
 	}
-	
+
 	private void createInputComposite() {
-		
+
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new GridLayout(2, false));
 
@@ -108,7 +130,7 @@ public class InfoEditor extends EditorPart{
 		nameLabel.setLayoutData(labelData);
 		nameText.setText(student.getName());
 		nameText.setLayoutData(textData);
-		nameText.setEditable(false);
+		nameText.setSelection(nameText.getText().length());
 		groupLabel.setText("Group");
 		groupLabel.setLayoutData(labelData);
 		groupText.setText(student.getGroup().getName());
@@ -130,14 +152,13 @@ public class InfoEditor extends EditorPart{
 		resultText.setLayoutData(textData);
 		resultText.setEditable(false);
 	}
-	
+
 	private void createPhotoComposite() {
-		
+
 		Composite composite = new Composite(parent, SWT.NONE);
 		composite.setLayout(new FillLayout());
 
-		Image image = new Image(parent.getDisplay(),
-				student.getPhotoPath());	
+		Image image = new Image(parent.getDisplay(), student.getPhotoPath());
 		canvas = new Canvas(composite, SWT.NONE);
 		canvas.addPaintListener(listener -> {
 			if (image != null) {
@@ -148,21 +169,61 @@ public class InfoEditor extends EditorPart{
 
 	public void update() {
 		setPartName(student.getName());
-		
+
 		nameText.setText(student.getName());
 		groupText.setText(student.getGroup().getName());
 		adressText.setText(student.getAdress());
 		cityText.setText(student.getCity());
 		resultText.setText(String.valueOf(student.getResult()));
-		
-		Image image = new Image(parent.getDisplay(),
-				student.getPhotoPath());
+
+		Image image = new Image(parent.getDisplay(), student.getPhotoPath());
 		canvas.addPaintListener(listener -> {
 			if (image != null) {
 				listener.gc.drawImage(image, 0, 0, image.getBounds().width, image.getBounds().height, 3, 10, 200, 200);
 			}
 		});
 		canvas.redraw();
-		
+
+	}
+
+	private void addListeners() {
+		nameText.addModifyListener(listener -> {
+			if (!student.getName().equals(nameText.getText())) {
+				isDirty = true;
+			} else {
+				isDirty = false;
+			}
+			undoStack.add(0, nameText.getText());
+			firePropertyChange(PROP_DIRTY);
+		});
+		nameText.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				boolean isCtrlPressed = (e.stateMask & SWT.CTRL) > 0;
+				if (isCtrlPressed) {
+					boolean isShiftPressed = (e.stateMask & SWT.SHIFT) > 0;
+					if (!isShiftPressed && e.keyCode == 'z') {
+						undo();
+					} else if (isShiftPressed && e.keyCode == 'z') {
+						redo();
+					}
+				}
+			}
+		});
+	}
+
+	private void undo() {
+		if (undoStack.size() > 1) {
+			redoStack.add(0, undoStack.remove(0));
+			nameText.setText(undoStack.remove(0));
+		}
+		nameText.setSelection(nameText.getText().length());
+	}
+
+	private void redo() {
+		if (redoStack.size() > 0) {
+			nameText.setText(redoStack.remove(0));
+		}
+		nameText.setSelection(nameText.getText().length());
 	}
 }
