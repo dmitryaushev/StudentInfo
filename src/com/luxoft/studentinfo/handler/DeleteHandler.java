3e965253d009907f23996ccf96d728b8fb8335fd
DAushev <DAushev@DAushev-NB.luxoft.com>
2021-05-20 02:22:39 +0300

    Updated DeleteHandler

--- src/com/luxoft/studentinfo/handler/DeleteHandler.java
+++ src/com/luxoft/studentinfo/handler/DeleteHandler.java
@@ -1,35 +1,53 @@
 package com.luxoft.studentinfo.handler;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.luxoft.studentinfo.model.Entry;
+import com.luxoft.studentinfo.model.Group;
 import com.luxoft.studentinfo.model.Student;
 import com.luxoft.studentinfo.view.InfoEditorInput;
 import com.luxoft.studentinfo.view.ViewManager;
 
 public class DeleteHandler extends AbstractHandler {
-	
+
 	public final static String ID = "com.luxoft.studentinfo.Delete";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelection(event);
-		Entry entry = (Entry) selection.getFirstElement();
-		if (entry != null) {
+		Iterator<Entry> i = selection.iterator();
+		while (i.hasNext()) {
+			Entry entry = i.next();
 			entry.getParent().removeEntry(entry);
 			ViewManager.getInstance().getTreeViewer().refresh();
-		}
-		if (entry instanceof Student) {
-			Student student = (Student) entry;
-			IEditorPart part = HandlerUtil.getActiveSite(event).getPage().findEditor(new InfoEditorInput(student));		
-			HandlerUtil.getActiveSite(event).getPage().closeEditor(part, false);
+			if (entry instanceof Group) {
+				List<Entry> entries = ((Group) entry).getEntries();
+				for(Entry e : entries) {
+					closeEditor(e);
+				}
+			}
+			if (entry instanceof Student) {
+				closeEditor(entry);
+			}
 		}
 		return null;
+	}
+	
+	private void closeEditor(Entry entry) {
+		Student student = (Student) entry;
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IEditorPart part = page.findEditor(new InfoEditorInput(student));
+		page.closeEditor(part, false);
 	}
 
 }
